b4249849a16c4c2317d103e20d5e6935579bdb4b
William Davis <liamdavis@tuta.io>
2020-05-01 18:25:44 -0400

    Fixed desktop view of skill icons in About component. Need to do the same for the mobile section.

--- src/components/Sections/SectionToggles/AboutToggle.js
+++ src/components/Sections/SectionToggles/AboutToggle.js
@@ -37,42 +37,46 @@ const AboutToggle = ({ index, sectionData, mobileViewportPortrait }) => {
           id="skills-container"
           style={{ opacity, ...toggledProps }}
         >
-          <AnimatedIcon
-            name="html"
-            className="skill-icon"
-            fill="#4A1E07"
-            style={{ opacity, ...toggledProps }}
-          />
-          <AnimatedIcon
-            name="css"
-            className="skill-icon"
-            fill="#4A1E07"
-            style={{ opacity, ...toggledProps }}
-          />
-          <AnimatedIcon
-            name="javascript"
-            className="skill-icon"
-            fill="#4A1E07"
-            style={{ opacity, ...toggledProps }}
-          />
-          <AnimatedIcon
-            name="react"
-            className="skill-icon"
-            fill="#4A1E07"
-            style={{ opacity, ...toggledProps }}
-          />
-          <AnimatedIcon
-            name="node"
-            className="skill-icon"
-            fill="#4A1E07"
-            style={{ opacity, ...toggledProps }}
-          />
-          <AnimatedIcon
-            name="python"
-            className="skill-icon"
-            fill="#4A1E07"
-            style={{ opacity, ...toggledProps }}
-          />
+          <animated.div className="row" style={{ opacity, ...toggledProps }}>
+            <AnimatedIcon
+              name="html"
+              className="skill-icon"
+              fill="#4A1E07"
+              style={{ opacity, ...toggledProps }}
+            />
+            <AnimatedIcon
+              name="css"
+              className="skill-icon"
+              fill="#4A1E07"
+              style={{ opacity, ...toggledProps }}
+            />
+            <AnimatedIcon
+              name="javascript"
+              className="skill-icon"
+              fill="#4A1E07"
+              style={{ opacity, ...toggledProps }}
+            />
+          </animated.div>
+          <animated.div className="row" style={{ opacity, ...toggledProps }}>
+            <AnimatedIcon
+              name="react"
+              className="skill-icon"
+              fill="#4A1E07"
+              style={{ opacity, ...toggledProps }}
+            />
+            <AnimatedIcon
+              name="node"
+              className="skill-icon"
+              fill="#4A1E07"
+              style={{ opacity, ...toggledProps }}
+            />
+            <AnimatedIcon
+              name="python"
+              className="skill-icon"
+              fill="#4A1E07"
+              style={{ opacity, ...toggledProps }}
+            />
+          </animated.div>
         </animated.div>
       </animated.div>
       <animated.div
