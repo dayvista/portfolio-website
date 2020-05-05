e594caaf3d3ee6bc052d244fcc7d9abbd4f4012e
William Davis <liamdavis@tuta.io>
2020-05-05 15:55:51 -0400

    Continued working on carousel prototype.

--- src/components/Sections/SectionToggles/ProjectsToggle.js
+++ src/components/Sections/SectionToggles/ProjectsToggle.js
@@ -7,10 +7,41 @@ import "../../../styles/css/Sections/Projects.css";
 
 const AnimatedIcon = animated(Icon);
 
-const stroke = "#ddd6e5";
+const stroke = "#050406";
 
 const ProjectsToggle = ({ index, sectionData, mVP, tVP }) => {
-  const [carouselIndex, setCarouselIndex] = useState(1);
+  // useEffect(() => {
+  //   findCarouselWidth();
+  // }, []);
+
+  const findCarouselWidth = () => {
+    // const carouselImages = document.querySelectorAll(".carousel-slider img");
+
+    // let imgWidth = carouselImages[2].clientWidth;
+
+    // console.log(imgWidth);
+
+    // return imgWidth;
+    return;
+  };
+
+  const [carouselIndex, setCarouselIndex] = useState(2);
+
+  const modifyCarouselIndex = (event) => {
+    console.log(carouselIndex);
+    let counter = carouselIndex;
+    console.log(counter);
+
+    if (event.target.id === "back-arrow-icon") {
+      counter--;
+      setCarouselIndex(counter);
+    } else if (event.target.id === "next-arrow-icon") {
+      counter++;
+      setCarouselIndex(counter);
+    }
+
+    console.log(counter);
+  };
 
   const toggled = useSpring({
     opacity: index === `${sectionData.name}` ? 1 : 0,
@@ -23,75 +54,66 @@ const ProjectsToggle = ({ index, sectionData, mVP, tVP }) => {
     },
   });
 
-  const [carouselProps, carouselSpring] = useSpring(() => ({}));
-
-  const handleClick = (event) => {
-    console.log(document.querySelectorAll("img")[carouselIndex].id);
-
-    const carouselImages = document.querySelectorAll(".carousel-slider img");
-
-    const size = carouselImages[0].clientWidth;
-    let counter = carouselIndex;
-
-    carouselSpring({
-      transform: "translateX(" + -size * counter + "px)",
-    });
-
-    if (event.target.id === "back-arrow-icon") {
-      if (counter <= 0) return;
-
-      counter--;
-      carouselSpring({
-        transform: "translateX(" + -size * counter + "px)",
-      });
-      setCarouselIndex(counter);
-    } else if (event.target.id === "next-arrow-icon") {
-      if (counter >= carouselImages.length - 1) return;
-
-      counter++;
-      carouselSpring({
-        transform: "translateX(" + -size * counter + "px)",
-      });
-      setCarouselIndex(counter);
-    }
-
-    if (carouselImages[counter].id === "last-project") {
-      counter = carouselImages.length - 2;
-      setCarouselIndex(counter);
-
-      carouselSpring({
-        transform: "translateX(" + -size * counter + "px)",
-      });
-    } else if (carouselImages[counter].id === "first-project") {
-      counter = carouselImages.length - counter;
-      setCarouselIndex(counter);
-
-      carouselSpring({
-        transform: "translateX(" + -size * counter + "px)",
-      });
-    }
-  };
-
-  // const imgSpring = useSpring({
-  //   opacity: carouselIndex === document.querySelectorAll('img')[carouselIndex].key,
-  //   from: {
-  //     opacity: 0
-  //   }
-  // });
+  const { opacity, ...carouselProps } = useSpring({
+    opacity: index === "projects" ? 1 : 0,
+    from: { opacity: 0 },
+    delay: index === "projects" ? 1000 : 0,
+  });
 
   return (
     <animated.div id="projects-container" style={toggled}>
       <animated.div className="carousel-container">
-        <Icon name="back-arrow" stroke={stroke} handleClick={handleClick} />
-        <animated.div className="carousel-slider" style={carouselProps}>
-          <img key={0} id="last-project" src="../../temp/img/fire.jpg"></img>
-          <img key={1} id="project-1" src="../../temp/img/earth.jpg"></img>
-          <img key={2} id="project-2" src="../../temp/img/water.jpg"></img>
-          <img key={4} id="project-3" src="../../temp/img/wind.jpg"></img>
-          <img key={3} id="project-4" src="../../temp/img/fire.jpg"></img>
-          <img key={5} id="first-project" src="../../temp/img/earth.jpg"></img>
+        <animated.div
+          className="carousel-slider"
+          style={{ opacity, ...carouselProps }}
+        >
+          <animated.img
+            id="proj-3-clone"
+            src="../../temp/img/wind.jpg"
+          ></animated.img>
+          <animated.img
+            id="proj-4-clone"
+            src="../../temp/img/fire.jpg"
+          ></animated.img>
+          <animated.img
+            id="proj-1"
+            src="../../temp/img/earth.jpg"
+          ></animated.img>
+          <animated.img
+            id="proj-2"
+            src="../../temp/img/water.jpg"
+          ></animated.img>
+          <animated.img
+            id="proj-3"
+            src="../../temp/img/wind.jpg"
+          ></animated.img>
+          <animated.img
+            id="proj-4"
+            src="../../temp/img/fire.jpg"
+          ></animated.img>
+          <animated.img
+            id="proj-1-clone"
+            src="../../temp/img/earth.jpg"
+          ></animated.img>
+          <animated.img
+            id="proj-2-clone"
+            src="../../temp/img/water.jpg"
+          ></animated.img>
         </animated.div>
-        <Icon name="next-arrow" stroke={stroke} handleClick={handleClick} />
+        <animated.div className="arrow-container">
+          <AnimatedIcon
+            name="back-arrow"
+            stroke={stroke}
+            style={{ opacity, ...carouselProps }}
+            handleClick={modifyCarouselIndex}
+          />
+          <AnimatedIcon
+            name="next-arrow"
+            stroke={stroke}
+            style={{ opacity, ...carouselProps }}
+            handleClick={modifyCarouselIndex}
+          />
+        </animated.div>
       </animated.div>
     </animated.div>
   );
