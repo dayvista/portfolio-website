748c057f051e598253fe0871f9bc66bfe8c2d678
William Davis <liamdavis@tuta.io>
2020-06-08 08:08:05 -0400

    Changed file locations for project images and resume PDF files.

--- src/components/Sections/SectionToggles/ResumeToggle.js
+++ src/components/Sections/SectionToggles/ResumeToggle.js
@@ -1,9 +1,20 @@
 import React from "react";
+import { animated, useSpring } from "react-spring";
 
 import "../../../styles/css/Sections/Resume.css";
 
 const ResumeToggle = (props) => {
-  return <div>{/* Section content goes here */}</div>;
+  return (
+    <animated.div>
+      <animated.iframe
+        src=""
+        frameBorder="0"
+        scrolling="auto"
+        height="100%"
+        width="100%"
+      ></animated.iframe>
+    </animated.div>
+  );
 };
 
 export default ResumeToggle;
--- src/components/Sections/data/projectsData.js
+++ src/components/Sections/data/projectsData.js
@@ -1,20 +1,20 @@
 export default [
   {
     desktopSrc:
-      "https://raw.githubusercontent.com/dayvista/portfolio-website/master/src/temp/img/desktoptemplate.gif",
+      "https://raw.githubusercontent.com/dayvista/portfolio-website/master/src/img/projects/desktoptemplate.gif",
     mobileSrc:
-      "https://raw.githubusercontent.com/dayvista/portfolio-website/master/src/temp/img/mobiletemplate.gif",
+      "https://raw.githubusercontent.com/dayvista/portfolio-website/master/src/img/projects/mobiletemplate.gif",
     header: "Portfolio Website - liamdavis.dev",
     skillsText:
       "Technologies Used: HTML, CSS, JavaScript, React, & React Spring (React animation library)",
     aboutText:
-      "I built this website from scratch after imagining the general layout. Experimenting with various React animation libraries, I settled on React Spring. The code for each component was storyboarded, although each storyboard changed immensely during production. I had to essentially learn how to make the website as I was making it.",
+      "This website was built from scratch after the general layout was conceptualized in my head. I had never seen a website built in this way before. Having, at most, built simple websites in both React and Vanilla JavaScript, I kinda just made it up as I went. Experimenting with various React animation libraries, I eventually settled on using React Spring for it's fluidity and performance.",
   },
   {
     desktopSrc:
-      "https://raw.githubusercontent.com/dayvista/portfolio-website/master/src/temp/img/desktoptemplate.gif",
+      "https://raw.githubusercontent.com/dayvista/portfolio-website/master/src/img/projects/desktoptemplate.gif",
     mobileSrc:
-      "https://raw.githubusercontent.com/dayvista/portfolio-website/master/src/temp/img/mobiletemplate.gif",
+      "https://raw.githubusercontent.com/dayvista/portfolio-website/master/src/img/projects/mobiletemplate.gif",
     header: "Placeholder Project",
     skillsText: "Tech Used: none",
     aboutText: "This is a placeholder card for UI testing.",
