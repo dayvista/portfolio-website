3bc3220183ca20aadd6b44dd4c479c315ce834f8
William Davis <liamdavis@tuta.io>
2020-06-23 22:33:10 -0400

    Added 2 of my old projects to the ProjectsToggle section via projectsData.js.

--- src/components/Sections/data/projectsData.js
+++ src/components/Sections/data/projectsData.js
@@ -1,24 +1,34 @@
 export default [
   {
-    header: "Portfolio Website - liamdavis.dev",
+    header: "Calculator - calc-u-now.netlify.app",
+    projectLink: "https://calc-u-now.netlify.app",
+    skillsText: "Technologies Used: HTML, CSS, JavaScript, & React",
+    aboutText: "",
+    desktopSrc:
+      "https://raw.githubusercontent.com/dayvista/portfolio-website/master/src/img/projects/calculator/desktop-calculatorproject.gif",
+    mobileSrc:
+      "https://raw.githubusercontent.com/dayvista/portfolio-website/master/src/img/projects/calculator/mobile-calculatorproject.gif",
+  },
+  {
+    header: "Random Quote Generator - kanyewestquotegenerator.netlify.app",
+    projectLink: "https://kanyewestquotegenerator.netlify.app",
+    skillsText: "Technologies Used: HTML, CSS, JavaScript, & React",
+    aboutText: "",
+    desktopSrc:
+      "https://raw.githubusercontent.com/dayvista/portfolio-website/master/src/img/projects/randomquotegenerator/desktop-randomquotegeneratorproject.gif",
+    mobileSrc:
+      "https://raw.githubusercontent.com/dayvista/portfolio-website/master/src/img/projects/portfolio/mobile-randomquotegeneratorproject.gif",
+  },
+  {
+    header: "Portfolio - liamdavis.dev",
     projectLink: "https://liamdavis.dev",
     skillsText:
       "Technologies Used: HTML, CSS, JavaScript, React, & React Spring (React animation library)",
     aboutText:
       "My portfolio website (this one) was built from scratch after conceptualizing the general layout. I had never seen a website built in this way before. Having at most built simple websites in both React and Vanilla JavaScript, I kinda just made it up as I went. Experimenting with various React animation libraries, I eventually settled on using React Spring for it's visual fluidity and overall speed and performance.",
     desktopSrc:
-      "https://raw.githubusercontent.com/dayvista/portfolio-website/master/src/img/projects/desktop-portfolioproject.gif",
+      "https://raw.githubusercontent.com/dayvista/portfolio-website/master/src/img/projects/portfolio/desktop-portfolioproject.gif",
     mobileSrc:
-      "https://raw.githubusercontent.com/dayvista/portfolio-website/master/src/img/projects/mobile-portfolioproject.gif",
-  },
-  {
-    header: "Placeholder Project",
-    projectLink: "https://liamdavis.dev",
-    skillsText: "Tech Used: none",
-    aboutText: "This is a placeholder card for UI testing.",
-    desktopSrc:
-      "https://raw.githubusercontent.com/dayvista/portfolio-website/master/src/img/projects/desktop-portfolioproject.gif",
-    mobileSrc:
-      "https://raw.githubusercontent.com/dayvista/portfolio-website/master/src/img/projects/mobile-portfolioproject.gif",
+      "https://raw.githubusercontent.com/dayvista/portfolio-website/master/src/img/projects/portfolio/mobile-portfolioproject.gif",
   },
 ];
